8cafa77b4431dfd325c52fdfe893d453db060162
Seolheon <negnomy@gmail.com>
2024-04-29 21:01:54 +0500

    delete dead method "move"

--- vueStart/src/components/Todolist.vue
+++ vueStart/src/components/Todolist.vue
@@ -98,10 +98,6 @@
         }
     ])
 
-    const moveItem = (arr, fromIndex, toIndex) => {
-    arr.splice(toIndex, 0, arr.splice(fromIndex, 1)[0]);
-  };
-
   const toggleComplete = (task) => {
 
     task.isComplete = !task.isComplete;
